2e2e80879114e604467eb92af24c6978b184e0d7
mattlim95 <matthewlim95@gmail.com>
2017-05-03 01:41:32 +0100

    Adding NuGet Packages and some MainProject code

--- MainProgram/Program.cs
+++ MainProgram/Program.cs
@@ -2,14 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics.Contracts;
+using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 
-namespace MainProgram
+namespace EgmMyo
 {
     class Program
-    {
-        static void Main(string[] args)
+    { // listen on an assigned port for inbound messages
+        public static int IpPortNumber = 6510; //6510
+        public static bool exitProg = false;
+        static void Main()
         {
+            string line = null;
+            Sensor s = new Sensor();
+            MyoProgram m = new MyoProgram(s);
         }
     }
+
+
+
 }
