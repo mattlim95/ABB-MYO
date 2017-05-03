0b840d2a3c9ef650936871b50efeaed5356549ea
mattlim95 <matthewlim95@gmail.com>
2017-05-03 02:00:24 +0100

    Added Egm and MyoProgram. Also added Google Protocol Buffers 2.4 (this program does not support newer updates to this)

---
+++ MainProgram/MyoProgram.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Linq;
+using System.Diagnostics.Contracts;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Threading;
+using MyoSharp;
+using MyoSharp.Commands;
+using MyoSharp.Communication;
+using MyoSharp.Device;
+using MyoSharp.Discovery;
+using MyoSharp.Exceptions;
+using MyoSharp.Math;
+using MyoSharp.Poses;
+
+namespace EgmMyo
+{
+    #region Methods
+    class MyoProgram
+    {
+        #region Static References
+        // using YuMi datasheet axis and values, need to check
+        public int MaxMyoZ = 1018; // check this is mazimum z range, robot z direction
+        public int MinMyoZ = 94;
+        public int MaxMyoX = 680;
+        public int MinMyoX = 405;
+        public int MaxMyoY = 664;
+        public int MinMyoY = 451;
+        public double MaxMyo1 = 168.5;
+        public double MinMyo1 = -168.5;
+        public double MaxMyo2 = 43.5;
+        public double MinMyo2 = -143.5;
+        public double MaxMyo7 = 168.5;
+        public double MinMyo7 = -168.5;
+        public double MaxMyo3 = 80;
+        public double MinMyo3 = -123.5;
+        public double MaxMyo4 = 138;
+        public double MinMyo4 = -290;
+        public double MaxMyo5 = 138;
+        public double MinMyo5 = -88;
+        public double MaxMyo6 = 229;
+        public double MinMyo6 = -229;
+        public const int NUMBER_OF_SENSORS = 8;
+        #endregion
+        Sensor abbSensor = null;
+        OrientationDataEventArgs myoOrient = null;
+        static EventWaitHandle _waitHandle = new AutoResetEvent(false);
+        public MyoProgram(Sensor abbSensor)
+        {
+            this.abbSensor = abbSensor;
+            this.abbSensor.Start();
+            StartMyo2();
+        }
+
+        private void StartMyo2()
+        {
+            Console.WriteLine("1111111111111111111111111111111111111111111111111111111111111");
+            bool MyoConnected = false;
+            // create a hub that will manage Myo devices for us
+            using (var channel = Channel.Create(
+                ChannelDriver.Create(ChannelBridge.Create(),
+                MyoErrorHandlerDriver.Create(MyoErrorHandlerBridge.Create()))))
+            using (var hub = Hub.Create(channel))
+            {
+                // listen for when the Myo connects
+                hub.MyoConnected += (sender, e) =>
+                {
+                    Console.WriteLine("Myo {0} has connected!", e.Myo.Handle);
+                    e.Myo.Vibrate(VibrationType.Short);
+                    _waitHandle.Set();
+                    e.Myo.OrientationDataAcquired += Myo_OrientationDataAcquired;
+                };
+
+                // listen for when the Myo disconnects
+                hub.MyoDisconnected += (sender, e) =>
+                {
+                    Console.WriteLine("Oh no! It looks like {0} arm Myo has disconnected!", e.Myo.Arm);
+                    e.Myo.OrientationDataAcquired -= Myo_OrientationDataAcquired;
+                };
+
+                Console.WriteLine("2222222222222222222222222222222222222222222222222222222222222");
+
+                // start listening for Myo data
+                channel.StartListening();
+
+                ConsoleRunner.UserInputHub(hub);
+                _waitHandle.WaitOne();
+                abbSensor.Start();
+            }
+        }
+        #endregion
+        #region Event Handlers
+        public void Myo_OrientationDataAcquired(object sender, OrientationDataEventArgs e)
+        {
+            var multiplier = (float)(10);
+            var roll = (float)(e.Roll* multiplier);
+            var pitch = (float)(e.Pitch* multiplier);
+            var yaw = (float)(e.Yaw* multiplier);
+
+            abbSensor.axis1 = roll;
+            abbSensor.axis2 = pitch;
+            abbSensor.axis3 = yaw;
+
+            
+        }
+        #endregion
+    }
+    
+    static class ConsoleRunner
+    {
+        #region Methods
+        internal static void UserInputHub(IHub hub)
+        {
+            string userInput;
+            while (string.IsNullOrEmpty((userInput = Console.ReadLine())))
+            {
+                // uhhhhhh
+            }
+        }
+        #endregion
+    }
+
+
+}
--- MainProgram/Program.cs
+++ MainProgram/Program.cs
@@ -18,12 +18,128 @@ namespace EgmMyo
         public static bool exitProg = false;
         static void Main()
         {
-            string line = null;
             Sensor s = new Sensor();
             MyoProgram m = new MyoProgram(s);
         }
     }
 
+    public class Sensor
+    {
+        private Thread _sensorThread = null;
+        private UdpClient _udpServer = null;
+        private bool _exitThread = false;
+        private uint _seqNumber = 0;
+        private ManualResetEvent _sensorSignal;
 
+        public float axis1 { get; set; }
+        public float axis2 { get; set; }
+        public float axis3 { get; set; }
+        public void SensorThread()
+        {
+            try
+            {   // create an udp client and listen on any address and the port IpPortNumber
+                // allow sockets to be reused for local network transmissions
+                // needed for simulations 
+                var remoteEp = new IPEndPoint(IPAddress.Any, Program.IpPortNumber);
+                _udpServer = new System.Net.Sockets.UdpClient();
+                _udpServer.ExclusiveAddressUse = false;
+                _udpServer.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+                _udpServer.Client.Bind(remoteEp);
+
+                while (_exitThread == false)
+                {
+                    // Get message from robot
+                    var data = _udpServer.Receive(ref remoteEp);
+                    if (data != null)
+                    {
+                        // de-serialize inbound message from robot using Google Protocol Buffer
+                        EgmRobot robot = EgmRobot.CreateBuilder().MergeFrom(data).Build();
+
+                        // display inbound message
+                        DisplayInboundMessage(robot);
+
+                        // create a new outbound sensor message
+                        EgmSensor.Builder sensor = EgmSensor.CreateBuilder();
+                        CreateSensorMessage(sensor);
+
+                        using (MemoryStream memoryStream = new MemoryStream())
+                        {
+                            EgmSensor sensorMessage = sensor.Build();
+                            sensorMessage.WriteTo(memoryStream);
+
+                            // send the udp message to the robot
+                            int bytesSent = _udpServer.Send(memoryStream.ToArray(), (int)memoryStream.Length, remoteEp);
+                            if (bytesSent < 0)
+                            { Console.WriteLine("Error send to robot"); }
+                        }
+
+
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error in receive Sockets : " + e.Message); Console.WriteLine(e.Data);
+                Console.WriteLine(e.HResult); Console.WriteLine(e.Source); Console.WriteLine(e.StackTrace);
+            }
+
+        }
+        // Display message from robot
+        void DisplayInboundMessage(EgmRobot robot)
+        {
+            if (robot.HasHeader && robot.Header.HasSeqno && robot.Header.HasTm)
+            {
+                Console.WriteLine("Seq={0} tm={1}", robot.Header.Seqno.ToString(), robot.Header.Tm.ToString());
+            }
+            else
+            {
+                Console.WriteLine("No header in robot message");
+            }
+        }
+        // Create a sensor message to send to the robot
+        void CreateSensorMessage(EgmSensor.Builder sensor)
+        {
+            // create a header
+            EgmHeader.Builder hdr = new EgmHeader.Builder();
+            hdr.SetSeqno(_seqNumber++).SetTm((uint)DateTime.Now.Ticks).SetMtype(EgmHeader.Types.MessageType.MSGTYPE_CORRECTION);
+            // Timestamp in milliseconds , sent by sensor, MSGTYPE_DATA if sent from robot controller
+            sensor.SetHeader(hdr);
+            // create some sensor data
+            EgmPlanned.Builder planned = new EgmPlanned.Builder();
+            EgmPose.Builder pos = new EgmPose.Builder();
+            EgmQuaternion.Builder pq = new EgmQuaternion.Builder();
+            EgmCartesian.Builder pc = new EgmCartesian.Builder();
+            EgmJoints.Builder pj = new EgmJoints.Builder();
+            pj.Build();
+            // use this to set systematic changes to coordinates
+            pj.AddJoints(1).AddJoints(2).AddJoints(3);
+            pj.SetJoints(0, this.axis1);
+            pj.SetJoints(1, this.axis2);
+            pj.SetJoints(2, this.axis3);
+            planned.SetJoints(pj); // bind joints object to planned
+            sensor.SetPlanned(planned); // bind planned to sensor obj
+                                        // display sensor data
+            DisplaySensorMessage(this.axis1, this.axis2, this.axis3);
+            return;
+        }
+        void DisplaySensorMessage(float roll, float pitch, float yaw)
+        {
+            Console.WriteLine("Roll={0} Pitch={1} Yaw={2}", roll, pitch, yaw);
+
+        }
+        // Start a thread to listen on inbound messages
+        public void Start()
+        {
+            _sensorThread = new Thread(new ThreadStart(SensorThread));
+            _sensorThread.Start();
+        }
+        // Stop and exit thread
+        public void Stop()
+        {
+            _exitThread = true;
+            _sensorThread.Abort();
+        }
+        // Suspend thread
+    }   
 
 }
